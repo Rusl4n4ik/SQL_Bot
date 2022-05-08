0651f3f0e4d3e44fa2a81b127a710057a17c3dfe
Rusl4n4ik <97291155+Rusl4n4ik@users.noreply.github.com>
2022-05-08 12:20:42 +0500

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,13 +1,10 @@
 from aiogram import types
+from db import get_buttons
 
-button_links = {'Очень крутой фонк': 'https://www.youtube.com/watch?v=W9GXqCBAZb0',
-                'Рецепт шавухи': 'https://www.youtube.com/watch?v=-rLg9VcEzxw',
-                'Что-то дорогое': 'https://www.youtube.com/watch?v=NqjKpE7bdXc',
-                'ЕГЭ': 'https://www.youtube.com/watch?v=jxiyffY8-e4'}
-
-start_menu = types.InlineKeyboardMarkup(row_width=2)
-start_buttons = [types.InlineKeyboardButton(x, url=button_links[x]) for x in button_links]
-start_menu.add(*start_buttons)
 admin_menu = types.InlineKeyboardMarkup(row_width=2)
 admin_menu.add(types.InlineKeyboardButton('Добавить кнопку', callback_data='add_button'))
+keyboard_m = types.ReplyKeyboardMarkup(resize_keyboard=True)
+new_btns = types.InlineKeyboardMarkup(row_width=2)
+button = ['Добавить кнопку']
+keyboard_m.add(*button)
 
