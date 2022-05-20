8d4712315615ce4bf9344dd3f6a156e15fffbb87
Rusl4n4ik <97291155+Rusl4n4ik@users.noreply.github.com>
2022-05-20 16:40:55 +0500

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -5,6 +5,6 @@ admin_menu = types.InlineKeyboardMarkup(row_width=2)
 admin_menu.add(types.InlineKeyboardButton('Добавить кнопку', callback_data='add_button'))
 keyboard_m = types.ReplyKeyboardMarkup(resize_keyboard=True)
 new_btns = types.InlineKeyboardMarkup(row_width=2)
-button = ['Добавить кнопку']
+button = ['Add button ➕']
 keyboard_m.add(*button)
 
