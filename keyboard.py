563cbd1f838ea82837d7d186f221f301d430fc2e
Rusl4n4ik <97291155+Rusl4n4ik@users.noreply.github.com>
2022-05-24 08:11:51 +0500

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,7 +2,7 @@ from aiogram import types
 from db import get_buttons
 
 admin_menu = types.InlineKeyboardMarkup(row_width=2)
-admin_menu.add(types.InlineKeyboardButton('Добавить кнопку', callback_data='add_button'))
+admin_menu.add(types.InlineKeyboardButton('Add new button', callback_data='add_button'))
 keyboard_m = types.ReplyKeyboardMarkup(resize_keyboard=True)
 new_btns = types.InlineKeyboardMarkup(row_width=2)
 button = ['Add button ➕']
